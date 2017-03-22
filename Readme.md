2f5fad715921eb8d3bf283307531ebe827a6ca26
rivlev <proflevitan@gmail.com>
2017-03-22 02:23:17 -0400

    personality task

--- Readme.md
+++ Readme.md
@@ -3,8 +3,10 @@
 ## Data
 myPersonality is a sample of personality scores and Facebook profile data collected by David Stillwell and Michal Kosinski through a Facebook application that implements the NEO-PI-R (five factor personality test, Costa & McCrae) and other psychological tests. This dataset is a subset containing 250 users and about 9900 status updates. It includes Facebook statuses in raw text, gold standard personality labels both as scores and as classes derived froma median split of the scores. It also includes several social network measures such as: network size, betweenness centrality, density, brokerage, transitivity.
 
-The status updates in myPersonality have been anonymized manually, with each proper name replaced by a fixed string (*PROPNAME*), though famous named entities such as Chopin or New York have not been replaced.
+The status updates in myPersonality have been anonymized manually, with each proper name replaced by a fixed string (\*PROPNAME\*), though famous named entities such as Chopin or New York have not been replaced.
 
-The data has been split into 80% train and 20% test. You will receive the training subset. The class distributions of the training data are as follows:
+The data has been split into 80% train and 20% test. You will receive the training subset. The class distributions of the training data are shown in train_splits.png; the test data is similarly imbalanced.
 
-<img src="train_splits.png" height="100" width="100">
+## Classification
+
+personality_classifier.py will load the data, run the feature extractor with the supplied configuration file, and train a model to predict each personality trait's binary label  with the extracted features. 
--- personality_classifier.py
+++ personality_classifier.py
@@ -1,26 +1,25 @@
 # label a status update for personality
 
-from sklearn.model_selection import cross_val_score
-from sklearn import svm
+from sklearn.model_selection import cross_val_score, cross_val_predict
+from sklearn import svm, preprocessing, metrics
 import feature_extractor as fe
 import numpy
 import re
 import sys
 import csv
 import codecs
+import argparse
+import pickle
+from subprocess import call
 
-datafile = sys.argv[1]
-conffile = sys.argv[2]
+class_idx = [7, 8, 9, 10, 11]
 
-ndata = -1 # for testing feature extraction: optional arg to control how much of data to use. won't work for testing classification because it just takes the first n -- all one class
-if len(sys.argv) > 3:
-	ndata = int(sys.argv[3])
-
-def load_data():
+def load_data(datafile):
 	with codecs.open(datafile, encoding="latin-1") as f:
-		return list(csv.reader(f))[1:ndata]
+		reader = csv.reader(f)
+		return next(reader), list(reader)
 
-def load_conf_file():
+def load_conf_file(conffile):
 	conf = set(line.strip() for line in open(conffile))
 	return conf
 
@@ -28,12 +27,36 @@ def predict_trait(X, Y):
 	scores = cross_val_score(svm.SVC(), X, Y, scoring='accuracy', cv=10)
 	return scores.mean()
 
+def handle_args():
+	parser = argparse.ArgumentParser()
+	parser.add_argument('-d', '--datafile', help='file containing data for training or testing', required=True, dest='datafile')
+	parser.add_argument('-c', '--conffile', help='file containing list of features to be extracted', dest='conffile', required=True)
+	parser.add_argument('-e', '--expdir', help='directory for storing conf file and models associated with this experiment', required=True)
+	parser.add_argument('-l', '--load', action='store_true', help='include to load models from <expdir> instead of training new')
+	return vars(parser.parse_args())
+
 if __name__ == "__main__":
-	data = load_data()
-	conf = load_conf_file()
-	labels = numpy.asarray([line[7:11] for line in data]).T.tolist()
-	print(numpy.array(labels).shape)
+	args = handle_args()
+	print(args)
+	header, data = load_data(args['datafile'])
+	labels = numpy.asarray([[line[i] for i in class_idx] for line in data]).T.tolist()
+
+	conf = load_conf_file(args['conffile'])
 	features = fe.extract_features([line[1] for line in data], conf)	# this will only pass the status update text to the feature extractor
 
-	for i in range(len(labels)):
-		print (predict_trait(features, labels[i]))
+	if not args['load']:
+		# train new models, evaluate, store
+		for i in range(len(class_idx)):
+			trait = header[class_idx[i]]
+			clf = svm.SVC().fit(features, labels[i])
+			predicted = cross_val_predict(clf, features, labels[i], cv=10)
+			print("%s: %.2f" % (header[class_idx[i]], metrics.accuracy_score(labels[i], predicted)))
+			with open("%s/%s.pkl" % (args['expdir'], trait), 'wb') as f:
+				pickle.dump(clf, f)
+	else:
+		for i in range(len(class_idx)):
+			trait = header[class_idx[i]]
+			with open("%s/%s.pkl" % (args['expdir'], trait), 'rb') as f:
+				clf = pickle.load(f)
+			print("%s: %.2f" % (trait, clf.score(features, labels[i])))
+		
